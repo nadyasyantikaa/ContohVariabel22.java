9d229fd42fd0f387c4fe172ec9c2ce451ed2d313
nadyasyantikaa <nadiasantikanaraya@gmail.com>
2024-10-04 10:08:12 +0700

    masukkan bank22

---
+++ ContohVariabel22.java
@@ -0,0 +1,14 @@
+public class ContohVariabel22 {
+    public static void main (String[] args) {
+        String salahSatuHobySayaAdalah = "Menonton film";
+        boolean isPandai = true;
+        char jenisKelamin = 'P';
+        byte _umurSayaSekarang = 19;
+        double $ipk = 3.9, tinggi = 1.57;
+        System.out.println(salahSatuHobySayaAdalah);
+        System.out.println("Apakah pandai? " + isPandai);
+        System.out.println("Jenis kelamin: " + jenisKelamin);
+        System.out.println("Umurku saat ini: " +_umurSayaSekarang);
+        System.out.println(String.format("Saya beripk %s, dengan tinggi badan %s", $ipk, tinggi));
+    }
+}
